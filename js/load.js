ac4c65f764ac41bee225187c562754904a88413d
Sidharth Nayyar <sidharthnayyar1@hotmail.com>
2017-08-11 14:25:18 -0700

    disabled automatic pause feature

--- js/load.js
+++ js/load.js
@@ -2,6 +2,7 @@ export default (game) => ({
   preload: function() {
     var loadingLabel = game.add.text(80, 150, 'loading...',
     {font: '30px Courier', fill: '#ffffff'})
+    game.stage.disableVisibilityChange = true;
     game.load.image('homeScreen', './images/HomeScreen1.png')
     game.load.image('player', './images/65938.png');
     game.load.image('ball', './images/aqua-ball.png');
