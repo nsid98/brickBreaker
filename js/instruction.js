1c1718d22cc4d4b16ce6ea6d3862ca04881500fa
Sidharth Nayyar <sidharthnayyar1@hotmail.com>
2017-08-29 14:55:41 -0700

    fix styling

--- js/instruction.js
+++ js/instruction.js
@@ -9,13 +9,22 @@ export default (game, Phaser) => ({
     var nameLabel = game.add.text(game.world.width / 2 -130, 20, 'Instructions',
     {font: '50px Arial', fill: '#ffffff'});
 
-    var startLabel = game.add.text(100, 80, 'Use the A and D or left and right arrow keys to move around',
+    var startLabel = game.add.text(100, 180, '-Use the A and D or left and right arrow keys to move to the sides',
     {font: '25px Arial', fill: '#ffffff'})
 
-    var startLabel = game.add.text(100, 120, 'Use spacebar to release a sticky ball ',
+    var startLabel = game.add.text(100, 220, 'to clear all the bricks',
     {font: '25px Arial', fill: '#ffffff'})
 
-    var startLabel = game.add.text(100, 160, 'Hit P to pause, and R to resume ',
+    var startLabel = game.add.text(100, 260, '-Use spacebar to release a sticky ball ',
+    {font: '25px Arial', fill: '#ffffff'})
+
+    var startLabel = game.add.text(100, 300, '-Hit P to pause, and R to resume ',
+    {font: '25px Arial', fill: '#ffffff'})
+
+    var startLabel = game.add.text(100, 400, '-To play in Multiplayer mode, enter it and wait for another player to join ',
+    {font: '25px Arial', fill: '#ffffff'})
+
+    var startLabel = game.add.text(100, 440, 'Then, work together to clear all the bricks! ',
     {font: '25px Arial', fill: '#ffffff'})
 
     var startLabel = game.add.text(280, game.world.height - 100, 'To go back to Home Screen, click here',
--- js/menu.js
+++ js/menu.js
@@ -1,18 +1,16 @@
 window.socket = io()
 export default (game, Phaser) => ({
   create: function () {
-    let homeScreen = game.add.sprite(0, 0, 'homeScreen');
-    homeScreen.width = game.width
-    homeScreen.height = game.height
+    let homeScreen = game.add.sprite(-50, 100, 'homeScreen');
 
-    this.game.singlePlayerBorder = game.add.sprite(75, 75, 'border')
+    this.game.singlePlayerBorder = game.add.sprite(335, 495, 'border')
     this.game.singlePlayerBorder.width = 240
     this.game.singlePlayerBorder.height = 35
     this.game.singlePlayerBorder.inputEnabled = true;
     this.game.singlePlayerBorder.alpha = 1
 
 
-    this.game.multiplayerBorder = game.add.sprite(555, 75, 'border')
+    this.game.multiplayerBorder = game.add.sprite(295, 545, 'border')
     this.game.multiplayerBorder.width = 335
     this.game.multiplayerBorder.height = 35
     this.game.multiplayerBorder.inputEnabled = true;
@@ -25,14 +23,10 @@ export default (game, Phaser) => ({
     this.game.instructionsBorder.inputEnabled = true;
     this.game.instructionsBorder.alpha = 1
 
-
-    var nameLabel = game.add.text(game.world.width / 2 -130, 20, 'Break Me',
-    {font: '50px Arial', fill: '#ffffff'});
-
-    var startLabel = game.add.text(80, 80, 'Click here to go solo',
+    var startLabel = game.add.text(340, 500, 'Click here to go solo',
     {font: '25px Arial', fill: '#ffffff'})
 
-    var startLabel = game.add.text(560, 80, 'Click here to play Multiplayer',
+    var startLabel = game.add.text(300, 550, 'Click here to play Multiplayer',
     {font: '25px Arial', fill: '#ffffff'})
 
     var instructionLabel = game.add.text(270, game.world.height - 80, 'Click here to go to instruction screen',
@@ -40,10 +34,10 @@ export default (game, Phaser) => ({
 
     game.input.onDown.add(function() {
 
-      if(game.input.mousePointer.x >= 80 && game.input.mousePointer.x < 305 && game.input.mousePointer.y >= 80 && game.input.mousePointer.y < 104){
+      if(game.input.mousePointer.x >= 335 && game.input.mousePointer.x < 575 && game.input.mousePointer.y >= 495 && game.input.mousePointer.y < 520){
       game.state.start('play')
       }
-      else if(game.input.mousePointer.x >= 560 && game.input.mousePointer.x < 900 && game.input.mousePointer.y >= 80 && game.input.mousePointer.y < 104){
+      else if(game.input.mousePointer.x >= 295 && game.input.mousePointer.x < 630 && game.input.mousePointer.y >= 545 && game.input.mousePointer.y < 580){
       socket.emit('MultiplayerStart')
       }
       else if(game.input.mousePointer.x >= 270 && game.input.mousePointer.x < 675 && game.input.mousePointer.y >= 600 && game.input.mousePointer.y < 625){
